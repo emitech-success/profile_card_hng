b188f081bfee32a9a29d8f2f8faf57cca3c81f0f
emitech-success <odezuluaugustine@gmail.com>
2025-01-30 03:24:52 +0100

    changed border color and also changed the text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateUTCTime() {
   const utcTimeElement = document.getElementById("utcTime");
   const now = new Date();
-  utcTimeElement.textContent = "UTC Time: " + now.toUTCString();
+  utcTimeElement.textContent =  now.toUTCString();
 }
 updateUTCTime();
